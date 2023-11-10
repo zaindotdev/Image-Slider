7e1b45f643f4390599c32dd41a43deb71fb6a4e2
Zain180 <114417655+Zain180@users.noreply.github.com>
2023-11-10 08:29:09 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-// let slideIndex = 0;
-// showSlides(slideIndex);
-
-// function showSlides(n) {
-//     const slides = document.querySelectorAll('.slide');
-//     if (n >= slides.length) {
-//         slideIndex = 0;
-//     }
-//     if (n < 0) {
-//         slideIndex = slides.length - 1;
-//     }
-//     const offset = -slideIndex * 100;
-//     document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
-// }
-
-// // Automatically advance the slider every 3 seconds (3000 milliseconds)
-// setInterval(function () {
-//     slideIndex++;
-//     showSlides(slideIndex);
-// }, 3000);
-
 let slides = document.querySelectorAll(".slide")
 let slideIndex = 0;
 slideShow(slideIndex);
